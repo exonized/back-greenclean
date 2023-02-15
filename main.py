6ba5ca0a4a07c31d70b9f0b63466f8a675173049
Anthony Flori <aflori@wearehunty.fr>
2023-02-15 20:26:19 +0100

    Starlette middleware cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 import fastapi.security as _security
 import fastapi
 
