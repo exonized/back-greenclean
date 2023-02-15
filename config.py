f54e8146b26c3135ecff8038a5b3e679ce298583
Anthony Flori <aflori@wearehunty.fr>
2023-02-15 20:34:20 +0100

    cors

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
 import fastapi.security as _security
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "https://gitlab.com/",
-    "https://green-back.herokuapp.com/",
-    "https://green-back.herokuapp.com/docs",
-    "https://greenclean.vercel.app/"
-]
 
 oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/api/token")
 JWT_SECRET = "myjwtsecret"
